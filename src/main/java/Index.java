a7993ee7ec78657fcbf60ef9b1f57e828eef2a7f
Lin Li Oechsle <tui20361@temple.edu>
2021-09-19 19:23:02 -0400

    added proper imports for spotify api java wrapper

--- src/main/java/Index.java
+++ src/main/java/Index.java
@@ -1,7 +1,17 @@
 import java.util.*;
+import com.wrapper.spotify.SpotifyApi;
+import com.wrapper.spotify.SpotifyHttpManager;
+import com.wrapper.spotify.exceptions.SpotifyWebApiException;
+import com.wrapper.spotify.model_objects.credentials.AuthorizationCodeCredentials;
+import com.wrapper.spotify.requests.authorization.authorization_code.AuthorizationCodeRequest;
+import org.apache.hc.core5.http.ParseException;
 
 public class Index {
     public static void main(String[] arg) {
+        SpotifyApi spotifyApi = new SpotifyApi.Builder()
+                .setAccessToken("[your account token goes here]")
+                .build();
+
         System.out.println("Hello world!");
     }
 }
